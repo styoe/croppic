59835b84339d003ed8d72395540047e5cf79c46a
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2014-05-29 09:46:36 -0400

    PHP rotation processing

--- img_crop_to_file.php
+++ img_crop_to_file.php
@@ -4,17 +4,22 @@
 */
 
 $imgUrl = $_POST['imgUrl'];
+// original sizes
 $imgInitW = $_POST['imgInitW'];
 $imgInitH = $_POST['imgInitH'];
+// resized sizes
 $imgW = $_POST['imgW'];
 $imgH = $_POST['imgH'];
+// offsets
 $imgY1 = $_POST['imgY1'];
 $imgX1 = $_POST['imgX1'];
+// crop box
 $cropW = $_POST['cropW'];
 $cropH = $_POST['cropH'];
+// rotation angle
 $angle = $_POST['rotation'];
 
-$jpeg_quality = 100;
+$png_quality = 9;
 
 $output_filename = "temp/croppedImg_".rand();
 
@@ -39,24 +44,34 @@ switch(strtolower($what['mime']))
     default: die('image type not supported');
 }
 
-	$resizedImage = imagecreatetruecolor($imgW, $imgH);
-	imagecopyresampled($resizedImage, $source_image, 0, 0, 0, 0, $imgW,
-				$imgH, $imgInitW, $imgInitH);
+    // resize the original image to size of editor
+    $resizedImage = imagecreatetruecolor($imgW, $imgH);
+	imagecopyresampled($resizedImage, $source_image, 0, 0, 0, 0, $imgW, $imgH, $imgInitW, $imgInitH);
 
+    // rotate the rezized image
+    $rotated_image = imagerotate($resizedImage, -$angle, 0);
+    // find new width & height of rotated image
+    $rotated_width = imagesx($rotated_image);
+    $rotated_height = imagesy($rotated_image);
+    // diff between rotated & original sizes
+    $dx = $rotated_width - $imgW;
+    $dy = $rotated_height - $imgH;
 
-	$dest_image = imagecreatetruecolor($cropW, $cropH);
-	imagecopyresampled($dest_image, $resizedImage, 0, 0, $imgX1, $imgY1, $cropW,
-				$cropH, $cropW, $cropH);
+    // crop rotated image to fit into original rezized rectangle
+	$cropped_rotated_image = imagecreatetruecolor($imgW, $imgH);
+	imagecolortransparent($cropped_rotated_image, imagecolorallocate($cropped_rotated_image, 0, 0, 0));
+	imagecopyresampled($cropped_rotated_image, $rotated_image, 0, 0, $dx / 2, $dy / 2, $imgW, $imgH, $imgW, $imgH);
 
-	// image rotation
-    $rotated_image = imagerotate($dest_image, $angle, 0);
+	// crop image into selected area
+	$final_image = imagecreatetruecolor($cropW, $cropH);
+	imagecolortransparent($final_image, imagecolorallocate($final_image, 0, 0, 0));
+	imagecopyresampled($final_image, $cropped_rotated_image, 0, 0, $imgX1, $imgY1, $cropW, $cropH, $cropW, $cropH);
 
-	imagejpeg($rotated_image, $output_filename.$type, $jpeg_quality);
+	// finally output png image
+	imagepng($final_image, $output_filename.$type, $png_quality);
 
-	$response = array(
-			"status" => 'success',
-			"url" => $output_filename.$type
-		  );
-	 print json_encode($response);
-
-?>
+	$response = Array(
+	    "status" => 'success',
+	    "url" => $output_filename.$type
+    );
+	print json_encode($response);
